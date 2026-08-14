438a83b230dbb8a8db6b3cc19aec60056475beea
RatanSankpal <ratanrsankpal007@gmail.com>
2026-08-14 21:12:18 +0530

    Year is leap or not

--- 1.c
+++ 1.c
@@ -1,20 +1,47 @@
-#include<stdio.h>
+ #include <stdio.h>
+
+int isLeapYear(int year)
+{
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        return 1;
+    else
+        return 0;
+}
+
+void printDays(int month, int year)
+{
+    if (month == 1 || month == 3 || month == 5 || month == 7 ||
+        month == 8 || month == 10 || month == 12)
+        printf("Number of days = 31");
+    else if (month == 4 || month == 6 || month == 9 || month == 11)
+        printf("Number of days = 30");
+    else if (month == 2)
+    {
+        if (isLeapYear(year))
+            printf("Number of days = 29");
+        else
+            printf("Number of days = 28");
+    }
+    else
+        printf("Invalid month");
+}
 
 int main()
 {
-        int no1,no2,result;
-        printf("Enter 2 no.s= ");
-        scanf("%d%d",&no1,&no2);
+    int year, month;
 
-        if(no2 == 0 )
-        {
-         printf("Error : Division is not allowed");
-        }
-        else{
-        printf("Result");
-        }
+    printf("Enter year: ");
+    scanf("%d", &year);
 
-        result= no1/no2;
-        printf("Result = %d" ,result);
+    if (isLeapYear(year))
+        printf("%d is a Leap Year\n", year);
+    else
+        printf("%d is not a Leap Year\n", year);
 
+    printf("Enter month (1-12): ");
+    scanf("%d", &month);
+
+    printDays(month, year);
+
+    return 0;
 }
